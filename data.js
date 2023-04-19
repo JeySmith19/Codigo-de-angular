0dd291b3133cf665a92f1bc44d808779a555704c
Jarol <86609720+JeySmith19@users.noreply.github.com>
2023-04-19 01:01:45 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,27 @@
+module.exports = function () {
+  var data = {
+    pets: [
+      {
+        id_TipoMembresia:1,
+        FechaInicio: "2022-09-10",
+        FechaFin: "2023-09-10",
+      },
+      {
+        id_TipoMembresia:2,
+        FechaInicio: "2022-09-10",
+        FechaFin: "2023-09-10",
+      },
+      {
+        id_TipoMembresia:3,
+        FechaInicio: "2022-09-10",
+        FechaFin: "2023-09-10",
+      },
+      {
+        id_TipoMembresia:4,
+        FechaInicio: "2022-09-10",
+        FechaFin: "2023-09-10",
+      }
+    ]
+  }
+  return data
+}
